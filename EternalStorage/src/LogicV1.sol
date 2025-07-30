dc2b714aacbd8fec9e7f50360437ca8f2d0875c4
GaurangBrdv <gaurang1415@gmail.com>
2025-07-30 18:32:02 +0530

    Eternal Storage LogicV1 contract

---
+++ EternalStorage/src/LogicV1.sol
@@ -0,0 +1,46 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {EternalData} from "./DataStorage.sol";
+
+contract LogicV1 {
+    address public storageContract;
+
+    constructor(address _storageContract) {
+        storageContract = _storageContract;
+    }
+
+    // Setter functions
+
+    function setUintValue(uint256 _value) external {
+        bytes32 key = keccak256(abi.encodePacked("setUint", msg.sender));
+        EternalData(storageContract).setUint(key, _value);
+    }
+
+    function setAddressValue(address _value) external {
+        bytes32 key = keccak256(abi.encodePacked("setAddress", msg.sender));
+        EternalData(storageContract).setAddress(key, _value);
+    }
+
+    function setBoolValue(bool _value) external {
+        bytes32 key = keccak256(abi.encodePacked("setBool", msg.sender));
+        EternalData(storageContract).setBool(key, _value);
+    }
+
+    // Getter functions
+
+    function getUintValue() external view returns(uint256) {
+        bytes32 key = keccak256(abi.encodePacked("setUint", msg.sender));
+        return EternalData(storageContract).getUint(key);
+    }
+
+    function getAddressValue() external view returns(address) {
+        bytes32 key = keccak256(abi.encodePacked("setAddress", msg.sender));
+        return EternalData(storageContract).getAddress(key);
+    }
+
+    function getBoolValue() external view returns(bool) {
+        bytes32 key = keccak256(abi.encodePacked("setBool", msg.sender));
+        return EternalData(storageContract).getBool(key);
+    }
+}
