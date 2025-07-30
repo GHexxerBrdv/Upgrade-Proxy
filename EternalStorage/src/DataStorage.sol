e32a6360f86fc72189414d7a4c58a20254eb2a8f
GaurangBrdv <gaurang1415@gmail.com>
2025-07-30 18:19:30 +0530

    Eternal Storage Data Storage Contract

--- EternalStorage/src/DataStorage.sol
+++ EternalStorage/src/DataStorage.sol
@@ -3,4 +3,43 @@ pragma solidity ^0.8.0;
 
 contract EternalData {
     
+    
+    /**
+     * @notice The Eternal storage contract :- which stores the storage and state variables.
+     * @author Gaurang Bharadava.
+     */
+
+    // Mappings
+
+    mapping(bytes32 => uint256) private uintValue;
+    mapping(bytes32 => address) private addressValue;
+    mapping(bytes32 => bool) private boolValue;
+
+    // Setter functions
+
+    function setUint(bytes32 _key, uint256 _value) external {
+        uintValue[_key] = _value;
+    }
+
+    function setAddress(bytes32 _key, address _value) external {
+        addressValue[_key] = _value;
+    }
+
+    function setBool(bytes32 _key, bool _value) external {
+        boolValue[_key] = _value;
+    }
+
+    // Getter functions
+
+    function getUint(bytes32 _key) external view returns(uint256) {
+        return uintValue[_key];
+    }
+
+    function getAddress(bytes32 _key) external view returns(address) {
+        return addressValue[_key];
+    }
+
+    function getBool(bytes32 _key) external view returns(bool) {
+        return boolValue[_key];
+    }
 }
