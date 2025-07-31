b819e1891512a9c073870b0f5dc95177fcec73c9
GaurangBrdv <gaurang1415@gmail.com>
2025-07-31 16:52:32 +0530

    Eternal Storage DataStorage contract deployment script

---
+++ script/EternalStorage/DataStorage.s.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {Script, console2} from "forge-std/Script.sol";
+import {EternalData} from "../../src/EternalStorage/DataStorage.sol";
+
+contract EtenalDataScript is Script {
+
+    function run() external returns(EternalData) {
+
+        vm.startBroadcast();
+        EternalData data = new EternalData();
+        vm.stopBroadcast();
+        
+        return data;
+    }
+}
