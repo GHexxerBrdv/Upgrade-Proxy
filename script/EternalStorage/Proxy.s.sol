1adb72eea89dd003081369229ff2b7001d9d1b01
GaurangBrdv <gaurang1415@gmail.com>
2025-08-05 14:29:28 +0530

    Eternal Storage Proxy contract deployment script

---
+++ script/EternalStorage/Proxy.s.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {Script} from "forge-std/Script.sol";
+import {EternalProxy} from "../../src/EternalStorage/EternalProxy.sol";
+import {DevOpsTools} from "foundry-devops/src/DevOpsTools.sol";
+
+contract ProxyScript is Script {
+    EternalProxy proxy;
+
+    function run() external returns (EternalProxy) {
+        address implementation = DevOpsTools.get_most_recent_deployment("LogicV1", block.chainid);
+        vm.startBroadcast();
+        proxy = new EternalProxy(implementation);
+        vm.stopBroadcast();
+
+        return proxy;
+    }
+}
