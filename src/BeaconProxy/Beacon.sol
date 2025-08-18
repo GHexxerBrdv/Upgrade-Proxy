4633c517519220e2a4e68db187f7c623104d7777
GaurangBrdv <gaurang1415@gmail.com>
2025-08-18 15:59:10 +0530

    Beacon Proxy: Beacon contract and Proxy.

---
+++ src/BeaconProxy/Beacon.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {SlotLib} from "./lib/SlotLib.sol";
+
+contract Beacon {
+    using SlotLib for bytes32;
+
+    // bytes32(uint256(keccak256('eip1967.proxy.beacon')) - 1)
+    bytes32 private constant IMPLEMENTATION_SLOT = 0xa3f0ad74e5423aebfd80d3ef4346578335a9a72aeaee59ff6cb3582b35133d50;
+
+    address owner;
+
+    modifier onlyAdmin() {
+        if (msg.sender != owner) {
+            revert("Not Owner");
+        }
+        _;
+    }
+
+    constructor(address implementation, address admin) {
+        owner = admin;
+        IMPLEMENTATION_SLOT.setSlot(implementation);
+    }
+
+    function updateImpl(address impl) external onlyAdmin {
+        if (impl == address(0)) {
+            revert("Zero address");
+        }
+
+        IMPLEMENTATION_SLOT.setSlot(impl);
+    }
+
+    function getImplementation() external view returns (address) {
+        address impl = IMPLEMENTATION_SLOT.readSlot();
+
+        return impl;
+    }
+}
--- src/BeaconProxy/BeaconProxy.sol
+++ src/BeaconProxy/BeaconProxy.sol
@@ -1,10 +1,42 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+import {IBeacon} from "./interfaces/IBeacon.sol";
+
 contract BeaconProxy {
-    address public immutable beacon;
+    address private immutable beacon;
 
     constructor(address _beacon) {
         beacon = _beacon;
+    }
+
+    function _getBeacon() internal view returns (address) {
+        return beacon;
+    }
+
+    function _implementation() internal view returns (address) {
+        address impl = IBeacon(_getBeacon()).getImplementation();
+
+        return impl;
+    }
+
+    function _fallback() internal returns (bytes memory) {
+        address impl = _implementation();
+
+        (bool ok, bytes memory data) = impl.delegatecall(msg.data);
+
+        if (!ok) {
+            revert("Transaction failed");
+        }
+
+        return data;
+    }
+
+    fallback(bytes calldata /* data */ ) external payable returns (bytes memory) {
+        return _fallback();
+    }
+
+    receive() external payable {
+        _fallback();
     }
 }
